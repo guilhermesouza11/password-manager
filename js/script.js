1fc0774f2504d6cd65024d6782dd1cd612296834
Guilherme Souza <guilhermesouzadossantos11@gmail.com>
2026-02-28 22:57:41 -0300

    feat: Captura informações que o usuário quer salvar e exibe do console

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-let service = document.querySelector("#service")
+const form = document.querySelector("#myForm");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const dados = Object.fromEntries(formData); 
+
+    console.log(dados);
+})
