d4d6315d1ca72975d06bf2dd6f60492325d12079
Guilherme Souza <guilhermesouzadossantos11@gmail.com>
2026-03-01 01:09:12 -0300

    feat: Foi adicionado validação simples e limpa formulário após o submit

--- js/script.js
+++ js/script.js
@@ -2,9 +2,23 @@ const form = document.querySelector("#myForm");
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    getFormData();
+})
 
+function getFormData(){
     const formData = new FormData(form);
     const dados = Object.fromEntries(formData); 
 
-    console.log(dados);
-})
+    if(dados.nameservice.trim() === ""){
+        alert("Preencha o nome do serviço que deseja salvar!")
+    }else if(dados.useremail.trim() === ""){
+        alert("Preencha o campo com o Usário ou Email que deseja salvar!")
+    }else if(dados.password.trim() === ""){
+        alert("Preencha o campo da senha que deseja salvar!")
+    }else{
+        console.log(dados)
+    }
+
+    form.reset(); //Retorna todo o formulário no seu status inicial
+    form.nameservice.focus(); //Faz com que o foco seja no primeiro input logo após o envio
+}
